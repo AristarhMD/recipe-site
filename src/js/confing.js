a377cc6b1fc88feb4c897c61e49f9772abf36bf6
AristarhMD <victor.nani1996@gmail.com>
2024-01-29 20:29:56 +0200

    Initial commit

---
+++ src/js/confing.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '92d7bc24-fb5f-4694-8b53-20061c3f87d2';
+export const MODAL_CLOSE_SEC = 2.5;
